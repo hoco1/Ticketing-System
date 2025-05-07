9bcee73cd9399b314e647e2ae396a33a3c3f5ac4
hoco1 <hocohelper@gmail.com>
2025-05-07 10:21:25 +0330

    Remove UUID import

--- src/main/java/com/ticketing/service/ShowService.java
+++ src/main/java/com/ticketing/service/ShowService.java
@@ -7,7 +7,7 @@ import com.ticketing.repo.ShowRepo;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.UUID;
+
 
 public class ShowService {
     private ShowRepo showRepo;
